82be205f85343e34b95461b584eaab9a36a54c41
root <root@DESKTOP-V1AO4B2.localdomain>
2023-04-14 19:19:17 -0500

    project

---
+++ Invertech/src/main/java/com/example/invertech/InvertechApplication.java
@@ -0,0 +1,13 @@
+package com.example.invertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvertechApplication.class, args);
+    }
+
+}
